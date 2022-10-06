3c20cb3eb6937717bcb2ff52a82859200ee17916
nookworth <nooksack@gmail.com>
2022-10-06 13:55:08 -0600

    POST method working.

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-// const HTML = require("./Develop/Routes/HTML_Routes/index.js");
-// const API = require("./Develop/Routes/API_Routes");
 const data = require("./Develop/db/db.json");
 
 const PORT = 3001;
@@ -13,8 +11,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use("/", HTML);
-// app.use("/api", API);
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
+    }
+  });
+};
 
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "./Develop/public/notes.html"))
@@ -34,10 +46,16 @@ app.post("/api/notes", (req, res) => {
       note_id: uuidv4(),
     };
 
-    fs.appendFile("./Develop/db/db.json", JSON.stringify(newNote), (err) =>
-      err ? console.error(err) : console.log("Commit logged!")
-    );
-    res.json("Note added successfully!");
+    readAndAppend(newNote, "./Develop/db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+    console.log(response);
+    
   }
 });
 
